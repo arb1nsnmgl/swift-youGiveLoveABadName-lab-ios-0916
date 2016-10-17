f21d750cb7c082534f6f9c1152bf3e00609bea13
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-17 00:01:10 -0400

    Completed

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var isMaxed = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,8 +21,30 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         
+        if !isMaxed {
+            UIView.animate(withDuration: 0.6, delay: 0.0, usingSpringWithDamping: 0.38, initialSpringVelocity: 0.0, options: [], animations: ({
+                
+                self.imageView.transform = CGAffineTransform.identity
+                self.imageView.frame = self.view.frame
+                
+            }), completion: nil)
+            
+            isMaxed = true
+            
+        } else {
+            
+            UIView.animate(withDuration: 0.6, delay: 0.0, usingSpringWithDamping: 0.38, initialSpringVelocity: 0.0, options: [], animations: ({
+                
+                self.imageView.transform = CGAffineTransform(scaleX: 1, y: 0.5)
+                
+            }), completion: nil)
 
+            isMaxed = false
+        }
+        
+        
     }
-
+    
+    
+    
 }
-
